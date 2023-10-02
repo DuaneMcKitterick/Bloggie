6958f4d51d1145a9d8375e11aab0094ed010b8a6
DuaneMcKitterick <146754109+DuaneMcKitterick@users.noreply.github.com>
2023-10-02 12:38:27 -0400

    Add files via upload

---
+++ List.cshtml.cs
@@ -0,0 +1,27 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Pages.Admin.Blogs
+{
+    public class ListModel : PageModel
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        [BindProperty]
+        public List<BlogPost> BlogPosts { get; set; }
+        
+        public ListModel(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+
+        public async Task OnGet()
+        {
+            BlogPosts = await bloggieDbContext.BlogPosts.ToListAsync();
+        }
+    }
+}
